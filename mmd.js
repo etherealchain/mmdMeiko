06edf298b9a76d16538ee56f27a7596b3d50f689
etherealchain <dinosaurkiller_x@yahoo.com.tw>
2017-07-22 17:12:48 +0800

    light rotation

--- mmd.js
+++ mmd.js
@@ -19,137 +19,8 @@ let shader = {
         }
     ] ),
 
-    vertexShader: 
-    `
-        #define PHONG
-        varying vec3 vViewPosition;
-        #ifndef FLAT_SHADED
-            varying vec3 vNormal;
-        #endif
-
-        #include <common>
-        #include <uv_pars_vertex>
-        #include <uv2_pars_vertex>
-        #include <displacementmap_pars_vertex>
-        #include <envmap_pars_vertex>
-        #include <color_pars_vertex>
-        #include <fog_pars_vertex>
-        #include <morphtarget_pars_vertex>
-        #include <skinning_pars_vertex>
-        #include <shadowmap_pars_vertex>
-        #include <logdepthbuf_pars_vertex>
-        #include <clipping_planes_pars_vertex>
-
-        uniform mat4 textureMatrix;
-        varying vec4 mirrorCoord;
-
-        void main() {
-            #include <uv_vertex>
-            #include <uv2_vertex>
-            #include <color_vertex>
-            #include <beginnormal_vertex>
-            #include <morphnormal_vertex>
-            #include <skinbase_vertex>
-            #include <skinnormal_vertex>
-            #include <defaultnormal_vertex>
-
-            #ifndef FLAT_SHADED // Normal computed with derivatives when FLAT_SHADED
-                vNormal = normalize( transformedNormal );
-            #endif
-
-            #include <begin_vertex>
-            #include <displacementmap_vertex>
-            #include <morphtarget_vertex>
-            #include <skinning_vertex>
-            #include <project_vertex>
-            #include <logdepthbuf_vertex>
-            #include <clipping_planes_vertex>
-
-            vViewPosition = - mvPosition.xyz;
-            
-            #include <worldpos_vertex>
-            #include <envmap_vertex>
-            #include <shadowmap_vertex>
-            #include <fog_vertex>
-
-            mirrorCoord = textureMatrix * worldPosition;
-        }
-    `,
-
-    fragmentShader: 
-    `
-        #define PHONG
-        uniform vec3 diffuse;
-        uniform vec3 emissive;
-        uniform vec3 specular;
-        uniform float shininess;
-        uniform float opacity;
-        #include <common>
-        #include <packing>
-        #include <color_pars_fragment>
-        #include <uv_pars_fragment>
-        #include <uv2_pars_fragment>
-        #include <map_pars_fragment>
-        #include <alphamap_pars_fragment>
-        #include <aomap_pars_fragment>
-        #include <lightmap_pars_fragment>
-        #include <emissivemap_pars_fragment>
-        #include <envmap_pars_fragment>
-        #include <gradientmap_pars_fragment>
-        #include <fog_pars_fragment>
-        #include <bsdfs>
-        #include <lights_pars>
-        #include <lights_phong_pars_fragment>
-        #include <shadowmap_pars_fragment>
-        #include <bumpmap_pars_fragment>
-        #include <normalmap_pars_fragment>
-        #include <specularmap_pars_fragment>
-        #include <logdepthbuf_pars_fragment>
-        #include <clipping_planes_pars_fragment>
-
-        uniform sampler2D mirrorSampler;
-        varying vec4 mirrorCoord;
-
-        float blendOverlay(float base, float blend) {
-            return( base < 0.5 ? ( 2.0 * base * blend ) : (1.0 - 2.0 * ( 1.0 - base ) * ( 1.0 - blend ) ) );
-        }
-
-        void main() {
-            #include <clipping_planes_fragment>
-            vec4 diffuseColor = vec4( diffuse, opacity );
-            ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-            vec3 totalEmissiveRadiance = emissive;
-
-            #include <logdepthbuf_fragment>
-            #include <map_fragment>
-            #include <color_fragment>
-            #include <alphamap_fragment>
-            #include <alphatest_fragment>
-            #include <specularmap_fragment>
-            #include <normal_flip>
-            #include <normal_fragment>
-            #include <emissivemap_fragment>
-
-            // accumulation
-            #include <lights_phong_fragment>
-            #include <lights_template>
-
-            // modulation
-            #include <aomap_fragment>
-            
-            vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
-            #include <envmap_fragment>
-
-            vec4 mirrorValue = texture2DProj(mirrorSampler, mirrorCoord);
-            vec4 lightColor = vec4( outgoingLight, diffuseColor.a );
-            gl_FragColor = vec4(blendOverlay(lightColor.r, mirrorValue.r), blendOverlay(lightColor.g, mirrorValue.g), blendOverlay(lightColor.b, mirrorValue.b), 1.0);
-
-            #include <premultiplied_alpha_fragment>
-            #include <tonemapping_fragment>
-            #include <encodings_fragment>
-            #include <fog_fragment>
-        }
-    `
+    vertexShader: document.getElementById( 'floorVS' ).textContent,
+    fragmentShader: document.getElementById( 'floorFS' ).textContent
 };
 
 var container, stats, loaderUI, progressBlue;
@@ -169,7 +40,7 @@ let textureLoader = new THREE.TextureLoader();
 
 let mirror, floorMaterial;
 let crowd = [];
-let textureSize = 1024;
+let textureSize = 512;
 let screenRatio = 1920/1080;
 let screenHeight = 60;
 
@@ -186,28 +57,26 @@ function init() {
     screenCamera = new THREE.PerspectiveCamera( 30, screenRatio, 1, 2000 ); 
     screenCamera.position.set(0,10,0);
 
-    // scene
     scene = new THREE.Scene();
-
     rtTexture = new THREE.WebGLRenderTarget( textureSize, textureSize, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat } );
     screen = new THREE.Mesh(new THREE.PlaneBufferGeometry(screenRatio*screenHeight,screenHeight), new THREE.MeshPhongMaterial({color: 0xFFFFFF, map:rtTexture.texture}));
     screen.position.set(0,30,-100);
     scene.add(screen);
 
     createLight();
-    // createCrowd();
+    createCrowd();
 
     // renderer
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setClearColor( new THREE.Color( 0xBDBDBD ) );
+    renderer.setClearColor( new THREE.Color( 0x2b2b2b ) );
     renderer.shadowMapEnabled = true;
     renderer.shadowMapType = THREE.PCFSoftShadowMap;
     container.appendChild( renderer.domElement );
 
     effect = new THREE.OutlineEffect( renderer );
-    // mirror = new MirrorReflection( controlCamera, { clipBias: 0.003,textureWidth:textureSize, textureHeight:textureSize } );
+    mirror = new MirrorReflection( controlCamera, { clipBias: 0.003,textureWidth:textureSize, textureHeight:textureSize } );
 
     floorMaterial = new THREE.ShaderMaterial({
         vertexShader:shader.vertexShader,
@@ -215,14 +84,14 @@ function init() {
         uniforms:shader.uniforms,
         lights:true
     });
-    // floorMaterial.uniforms.mirrorSampler.value = mirror.renderTarget.texture;
-    // floorMaterial.uniforms.textureMatrix.value = mirror.textureMatrix;
+    floorMaterial.uniforms.mirrorSampler.value = mirror.renderTarget.texture;
+    floorMaterial.uniforms.textureMatrix.value = mirror.textureMatrix;
     floorMaterial.outlineParameters = {
         visible: false
     };
     
     circleFloor = new THREE.Mesh( new THREE.CircleBufferGeometry( 45, 64), floorMaterial );
-    // circleFloor.add( mirror );
+    circleFloor.add( mirror );
     circleFloor.rotateX( - Math.PI / 2 );
     circleFloor.receiveShadow = true;
     circleFloor.position.setY(-0.1);
@@ -303,7 +172,7 @@ function init() {
                 initGui();
                 animate();
                 moveLight();
-                // moveOtaku();
+                moveOtaku();
                 document.body.removeChild( loaderUI );
                 document.body.appendChild( container );
             }, onProgress3, onError );
@@ -432,7 +301,7 @@ function animate() {
 
 function render() {
     
-    if ( lightHelper1 ) lightHelper1.update();
+    // if ( lightHelper1 ) lightHelper1.update();
     // if ( lightHelper2 ) lightHelper2.update();
     // if ( lightHelper3 ) lightHelper3.update();
 
@@ -441,10 +310,10 @@ function render() {
     if ( ikHelper !== undefined && ikHelper.visible ) ikHelper.update();
 
     // render to screen
-    // effect.render( scene, screenCamera, rtTexture, true);
+    effect.render( scene, screenCamera, rtTexture, true);
     // render mirror
-    // mirror.updateTextureMatrix();
-    // renderer.render( scene, mirror.mirrorCamera, mirror.renderTarget, true);
+    mirror.updateTextureMatrix();
+    renderer.render( scene, mirror.mirrorCamera, mirror.renderTarget, true);
 
     effect.render( scene, controlCamera );
 }
@@ -466,11 +335,11 @@ function getRandomInt(min, max) {
 
 function createCrowd(){
     let innerRadius = 60;
-    let outerRadius = 80;
+    let outerRadius = 65;
     let spriteMap = textureLoader.load( 'data/person.png' );
     let spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
 
-    for(i = 0; i < 200; i++){
+    for(i = 0; i < 100; i++){
         let theta = Math.random()*Math.PI*2;
         let radius = getRandomArbitrary(innerRadius, outerRadius);
         createOtaku(radius*Math.cos(theta),0,radius*Math.sin(theta), spriteMaterial);
@@ -507,21 +376,19 @@ function moveOtaku(){
 
 
 // light
-var lightDegree = [];
 var lights = [];
 var lightMaterial;
 var lightRadius = 50;
 
 function moveLight(){
+    for(i = 0 ; i < lights.length; i++){
+        lights[i].degree = (lights[i].degree + 1)%360;
+        let radian = lights[i].degree*Math.PI/180;
 
-    // tween( spotLight1 );
-    // tween( spotLight2 );
-    // tween( spotLight3 );
-    lightdegree++;
-    let lightradian = lightdegree%360*Math.PI/180;
-    x = lightRadius*Math.cos(lightradian);
-    y = lightRadius*Math.sin(lightradian);
-    lightSource.position.set(x,y,0);
+        x = lightRadius*Math.cos(radian);
+        y = lightRadius*Math.sin(radian);
+        lights[i].children[0].position.set(x,y,0);
+    }
     setTimeout(moveLight, 20);
 }
 function createLight(){
@@ -533,20 +400,20 @@ function createLight(){
             scale: { type: "v3", value: new THREE.Vector3(5,5,1) },
             color: {value: new THREE.Color()}
         },
-        vertexShader: document.getElementById( 'distanceFieldvs' ).textContent,
-        fragmentShader: document.getElementById( 'distanceFieldfs' ).textContent,
+        vertexShader: document.getElementById( 'distanceFieldVS' ).textContent,
+        fragmentShader: document.getElementById( 'distanceFieldFS' ).textContent,
         transparent: true
     } );
 
-    let light = createPointLight(0xFF7F00);
-    light.position.set( 0, lightRadius, 0 );
-    lights.push(light);
-    light = createPointLight(0x00FF7F);
-    light.position.set( lightRadius, 0, 0 );
-    lights.push(light);
-    light = createPointLight(0x7F00FF);
-    light.position.set( -lightRadius, 0, 0 );
-    lights.push(light);
+    createPointLight(0xFF7F00);
+    createPointLight(0x00FF7F);
+    createPointLight(0x0DA2F2);
+
+    lights[0].degree = 0;
+    lights[1].rotateY(120*Math.PI/180);
+    lights[1].degree = 30;
+    lights[2].rotateY(240*Math.PI/180);
+    lights[2].degree = 60;
     
     // lightSource = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), material );
     // lightSource.position.set(0,50,0);
@@ -571,13 +438,17 @@ function createLight(){
 }
 
 function createPointLight(color){
-
-    let lightSource = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), lightMaterial );
-    let pointLight = new THREE.PointLight(color, 1, 100);
+    let material = lightMaterial.clone();
+    material.uniforms.color.value = new THREE.Color(color);
+    let lightSource = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), material );
+    let pointLight = new THREE.PointLight(color, 1, lightRadius+10);
+    let lightBase = new THREE.Object3D();
     pointLight.castShadow = true;
+
     lightSource.add(pointLight);
-    scene.add(lightSource);
-    return lightSource;
+    lightBase.add(lightSource);
+    scene.add(lightBase);
+    lights.push(lightBase);
 }
 
 function createSpotlight( color ) {
